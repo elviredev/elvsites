afa7031f0d58ab57bd8ab6e23996e4570b78202d
elviredev <sandrine.rodriguez@hotmail.com>
2024-12-09 13:48:09 +0100

    ✨ add tag Google for Google Analytics.

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html lang="fr">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FFBXPQ7QZP"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-FFBXPQ7QZP');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
